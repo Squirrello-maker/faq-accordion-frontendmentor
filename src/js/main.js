fc40f7be81e53dad988718ad35d1b362fe703126
Emil Olejnik <olejnike2000@gmail.com>
2021-08-11 19:18:56 +0200

    added behavior to arrows

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,22 @@
 const accordion = document.querySelector('.accordion');
 const accordionBtns = accordion.querySelectorAll('.accordion__item__btn');
 const accordionInfos = accordion.querySelectorAll('.accordion__item__info');
-
+const arrows = accordion.querySelectorAll('.fas');
 function openAccordionItem()
 {
     if(this.nextElementSibling.classList.contains('active-item'))
     {
         this.nextElementSibling.classList.remove('active-item');
         this.classList.remove('bold');
+        this.children[0].classList.remove('rotate');
     }
     else
     {    
         closeActiveItems();
         this.nextElementSibling.classList.toggle('active-item');
         this.classList.add('bold');
+        this.children[0].classList.add('rotate');
+
     }
 
     
@@ -27,6 +30,9 @@ const closeActiveItems = () => {
         {
             btn.classList.remove('bold');
         }    
+    });
+    arrows.forEach(arrow => {
+        arrow.classList.remove('rotate');
     })
 }
 accordionBtns.forEach(btn => {
